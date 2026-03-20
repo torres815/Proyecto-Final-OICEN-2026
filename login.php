88dc75fa8f91f1278a877438e6f072a74dea38d8
torres815 <torres815@cen.edu.ar>
2026-03-20 00:39:22 -0300

    avance de login

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_login.css">
+    <title>Login-Oicen</title>
+</head>
+<body>
+    <div class="contenedor_1">
+        <img class="logo-oicen" src="templates/logo-oicen.png" alt="Logo Oicen">
+    </div>
+    
+      <div class="formulario">
+                <div class="form-encabezado">
+                    <h2>Iniciar Sesión</h2>
+                    <p>Ingresa tus credenciales.</p>
+                </div>
+                <form>
+                    <div class="entrada">
+                        <label for="username">Usuario </label>
+                        <input type="text" id="username" placeholder="ejemplo@gmail.com" required>
+                    </div>
+                    <div class="entrada">
+                        <label for="password">Contraseña</label>
+                        <input type="password" id="password" placeholder="••••••••" required>
+                    </div>
+                    <div class="form-options">
+                    </div>
+                    <a href="index.php">
+                    <button>Entrar al Sistema</button>
+                    </a>
+                </form>
+</body>
+</html>
